826b18e39538870907dc72d6937bf793be768cdb
BenjaminLi15 <benjaminli423@gmail.com>
2020-07-30 14:43:41 -0400

    Strategy File Created It Was

---
+++ src/strategy_implement.py
@@ -0,0 +1,17 @@
+def strategy_0():
+    pass
+
+
+def multiply(x, y):
+    """
+        set a multiplication function
+    """
+    return x*y
+
+
+def divide(x, y):
+    """
+        set a division function
+    """
+    return x/y
+
