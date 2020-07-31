690f1e10343e0e42d6f138a53011bc604edb8834
Eugeneh1217 <Eugeneh1217@gmail.com>
2020-07-30 20:53:47 -0400

    added testing results evaluation to test.py

--- src/test.py
+++ src/test.py
@@ -4,34 +4,11 @@ from stock_market_env import StockMarketEnv
 from csv_parse import SECTOR_DICT
 import datetime as dt
 
-print(SECTOR_DICT["Basic_Materials_Sector"])
-
-
-def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            return False
-        return True
-    return False
-
-
-def has_leap_year(start_year, end_year):
-    leap_years = 0
-    for year in range(start_year, end_year + 1):
-        if is_leap_year(year):
-            leap_years += 1
-    return leap_years
-
-
 # list of strategy method references
 strategies = []
 
+# list of histories from testing loops:
 histories = []
-
-# list with days in each month
-days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 # constant and average_period ranges in days (inclusive)
 CONSTANT_RANGE = (0, 20)
@@ -47,15 +24,52 @@ env = StockMarketEnv(PRINCIPLE)
 N = 5
 
 # currently tests with investing daily
-for strategy in strategies:
-    for constant in range(CONSTANT_RANGE[0]. CONSTANT_RANGE[1] + 1):
-        for ave_period in range(AVE_PERIOD_RANGE[0], AVE_PERIOD_RANGE[1] + 1):
-            for testing_period in range(N):
-                today = dt.date.today()
+for strategy_index in range(len(strategies)):
+    for testing_period in range(N):
+
+        # reference to today bc it is used multiple times
+        today = dt.date.today()
+
+        for constant in range(CONSTANT_RANGE[0]. CONSTANT_RANGE[1] + 1):
+
+            for ave_period in range(AVE_PERIOD_RANGE[0], AVE_PERIOD_RANGE[1] + 1):
+
+                # reset day before running testing loop
                 day = today - dt.timedelta(days=N * 365)
+
+                # testing loop
                 while day != today:
+
+                    # increment day
                     day += dt.timedelta(days=1)
-                    buy_list, sell_list = strategy(day - dt.timedelta(days=ave_period), day)
+
+                    # impliment current strategy
+                    buy_list, sell_list = strategies[strategy_index](day - dt.timedelta(days=ave_period), day)
+
+                    # carry out transactions specified by strategy
                     env.step(buy_list, sell_list)
-                histories.append(env.history)
+
+                # record history
+                histories.append(dict(strategy=strategies[strategy_index].__name__, testing_period=testing_period,
+                                      constant=constant, ave_period=ave_period, history=env.history))
                 env.reset()
+
+
+# list to hold best strategy with best hyper parameters
+best_three = [histories[0], histories[1], histories[2]]
+
+# evaluate different hyperparameters for different strategies
+for trial_index in range(3, len(histories)):
+    last_networth = histories[trial_index]["history"][0][len(histories[trial_index]["history"]) - 1]
+    for x in range(len(best_three)):
+        if last_networth > best_three[x]:
+            best_three.insert(histories[trial_index])
+    best_three = best_three[:4]
+
+for trial in best_three:
+    print("strategy: {}".format(trial["strategy"]))
+    print("testing_period: {}".format(trial["testing_period"]))
+    print("constant: {}".format(trial["constant"]))
+    print("ave_period: {}".format(trial["ave_period"]))
+    print("end networth: {}".format(trial["history"][0][len(trial["history"][0]) - 1]))
+
