a677fd8f2308679187e6181a7712a93f962ae703
BenjaminLi15 <benjaminli423@gmail.com>
2020-07-31 21:21:29 -0400

    Updated Strategy_0 Implementation

--- src/strategy_implement.py
+++ src/strategy_implement.py
@@ -2,28 +2,37 @@ from csv_parse import SECTOR_DICT
 from comp_pe import compare_pe
 
 
-def strategy_0(date, constant, growth_average_start_date, growth_average_end_date):
+def strategy_0(date, constant, growth_average_start_date, growth_average_end_date, principle):
     """implement strategy_0 to return a two lists of tuples"""
 
     # calls compare_pe() on the entire parsed list: SECTOR_DICT
     # instantiate needed stuff
     dictionary_length = len(SECTOR_DICT)
     sector_list_holder = []
-    compare_pe_holder = []
     top_five = []
     last_five = []
+    pe_per_industry = []
     current = 0
     counter = 0
+    growth = "PERCENT"
 
     # run through the dictionary of lists
     for key in SECTOR_DICT.keys():
         """first part to find a list of the 5 best and 5 worst compare_pe() values"""
         sector_list_holder = SECTOR_DICT[key]
+        compare_pe_holder = []
 
         # append all compare_pe() values onto a compare_pe_holder
+        # compare_pe_holder is a nested list of (ticker_symbol, compare_pe value, industry)
         for i in sector_list_holder:
-            compare_pe_holder.append(sector_list_holder[i][0],
-                                     sector_list_holder[i].compare_pe())
+            # creates temporary triple list holder, to hold the above info
+            temporary_triple_list = (sector_list_holder[i][0],
+                                     compare_pe(sector_list_holder[i][0], key,
+                                                growth_average_start_date, growth_average_end_date, growth), key)
+            compare_pe_holder.append(temporary_triple_list)
+
+        # creates a nested list of tuples in format (industry, average_compare_pe)
+        pe_per_industry.append(average_pe_industry(compare_pe_holder))
 
         # find the top 5 compare_pe() values
         while current < (len(compare_pe_holder)):
@@ -37,7 +46,7 @@ def strategy_0(date, constant, growth_average_start_date, growth_average_end_dat
             # remove the lowest value if tuple added, should be able to be simplified but the nested lists make hard
             # look into min() function
             if len(top_five) == 6:
-                top_five = removelowest(top_five, 1)
+                top_five = remove_lowest(top_five, 1)
 
             counter = 0
             current += 1
@@ -52,17 +61,31 @@ def strategy_0(date, constant, growth_average_start_date, growth_average_end_dat
                 last_five.append(compare_pe_holder[current])
 
             if len(last_five) == 6:
-                last_five = removehighest(last_five, 1)
+                last_five = remove_highest(last_five, 1)
 
             counter = 0
             current += 1
 
-        """implement the strategy_0 formula to yield a list of [(ticker symbol), (# of stocks)]"""
+    """implement the strategy_0 formula to yield a list of [(ticker symbol), (# of stocks)]"""
+    # determine the buying and selling constants
+    BUYING_CONSTANT = constant * principle
+
 
     return top_five, last_five
 
+def average_pe_industry(nested_triple):
+    """returns a single list of length 2 in the format (industry, average_compare_pe)"""
 
-def removelowest(extended_list, value_column):
+    temporary_tuple = []
+    designated_industry = nested_triple[0][2]
+
+    for i in range(len(nested_triple)):
+
+
+    return temporary_tuple
+
+
+def remove_lowest(extended_list, value_column):
     """removes the lowest value of a nested list"""
 
     LOW = []
@@ -75,7 +98,7 @@ def removelowest(extended_list, value_column):
     return extended_list
 
 
-def removehighest(extended_list, value_column):
+def remove_highest(extended_list, value_column):
     """removes the highest value of a nested list"""
 
     MAX = []
